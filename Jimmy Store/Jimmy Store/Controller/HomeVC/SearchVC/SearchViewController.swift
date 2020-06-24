068d8e92ae6b1f7e45a9b7202f21fb4d0ce14ae7
shsx477 <shsx477@gmail.com>
2020-06-25 03:10:41 +0900

    move UINavigationController to MainTabBarController

---
+++ Jimmy Store/Jimmy Store/Controller/HomeVC/SearchVC/SearchViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SearchViewController.swift
+//  Jimmy Store
+//
+//  Created by 윤병일 on 2020/06/20.
+//  Copyright © 2020 Byoungil Youn. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController : UIViewController {
+    
+}
